52bb226a3d0fe9cd4f8a24539997fb9d127cee5b
Sorunome <mail@sorunome.de>
2020-06-25 17:25:29 +0200

    combind scraping from riot-web and riot x

--- scrape.js
+++ scrape.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
+const xml2json = require("xml2json");
 
 const REACT_SDK_PATH = "/home/sorunome/repos/matrix-react-sdk";
+const RIOT_X_PATH = "/home/sorunome/repos/riotX-android";
 
-const translationsPath = "/src/i18n/strings";
+const reactSdkTranslationsPath = "/src/i18n/strings";
+const riotXTranslationsPath = "/matrix-sdk-android/src/main/res";
 const emojiFile = "./sas-emoji.json";
 const outFile = "./sas-emoji-translations.json";
 const outFileLang = "./sas-emoji-languages.json";
@@ -12,28 +15,95 @@ const languages = [];
 
 const emoji = JSON.parse(fs.readFileSync(emojiFile));
 
-const dir = fs.readdirSync(REACT_SDK_PATH + translationsPath);
-for (const f of dir) {
-	if (!f.includes(".json")) {
-		continue;
+const reactSdkCache = {};
+function getEmoteReactSdk(lang, str) {
+	if (str === "Thumbs Up") {
+		str = "Thumbs up";
 	}
-	const langJson = JSON.parse(fs.readFileSync(REACT_SDK_PATH + translationsPath + "/" + f));
-	const lang = f.replace(".json", "");
+	if (str === "Light Bulb") {
+		str = "Light bulb";
+	}
+	if (!reactSdkCache[lang]) {
+		let file;
+		try {
+			file = fs.readFileSync(REACT_SDK_PATH + reactSdkTranslationsPath + "/" + lang + ".json");
+		} catch (err) {
+			return;
+		}
+		reactSdkCache[lang] = JSON.parse(file);
+	}
+	return reactSdkCache[lang][str];
+}
+
+function allReactSdkLangs() {
+	const langs = [];
+	const dir = fs.readdirSync(REACT_SDK_PATH + reactSdkTranslationsPath);
+	for (const f of dir) {
+		if (!f.includes(".json")) {
+			continue;
+		}
+		let lang = f.replace(".json", "");
+		langs.push(lang);
+	}
+	return langs;
+}
+
+const riotXCache = {};
+function getEmoteRiotX(lang, str) {
+	str = str.toLowerCase().replace(" ", "");
+	const strMap = {
+		"spanner": "wrench",
+		"aeroplane": "airplane",
+		"headphones": "headphone",
+	};
+	str = strMap[str] || str;
+	const langMap = {
+		"en_EN": "en_GB",
+	};
+	lang = langMap[lang] || lang;
+	lang = lang.replace("_", "-r");
+	if (!riotXCache[lang]) {
+		let file;
+		try {
+			let path = "/values-" + lang;
+			if (lang == "en-rUS") {
+				path = "/values";
+			}
+			file = fs.readFileSync(RIOT_X_PATH + riotXTranslationsPath + path + "/strings.xml");
+		} catch (err) {
+			return;
+		}
+		riotXCache[lang] = JSON.parse(xml2json.toJson(file));
+	}
+	try {
+		return riotXCache[lang].resources.string.find((e) => e.name === "verification_emoji_" + str)["$t"];
+	} catch (err) {
+		// dispose
+	}
+}
+
+function getEmote(lang, str) {
+	return getEmoteReactSdk(lang, str) || getEmoteRiotX(lang, str);
+}
+
+const langs = allReactSdkLangs();
+for (const lang of langs) {
 	const translation = [];
 	for (const e of emoji) {
-		if (e.description === "Thumbs Up") {
-			e.description = "Thumbs up";
+		let trans = getEmote(lang, e.description);
+		if (!trans && lang === "en_US") {
+			trans = getEmote("en_EN", e.description);
 		}
-		if (e.description === "Light Bulb") {
-			e.description = "Light bulb";
+		if (!trans && lang === "en_EN") {
+			trans = getEmote("en_US", e.description);
 		}
-		if (!langJson[e.description]) {
-			if (lang == "en_EN") {
-				console.log("MISSING:", e);
+		if (!trans) {
+			if (lang === "en_EN") {
+				console.log("MISSING EMOTE:", e);
 			}
 			continue;
 		}
-		translation.push(langJson[e.description]);
+		translation.push(trans);
 	}
 	if (translation.length != emoji.length) {
 		continue;
@@ -41,5 +111,7 @@ for (const f of dir) {
 	out[lang] = translation;
 	languages.push(lang);
 }
+
+console.log("Languages:", languages);
 fs.writeFileSync(outFile, JSON.stringify(out));
 fs.writeFileSync(outFileLang, JSON.stringify(languages));
