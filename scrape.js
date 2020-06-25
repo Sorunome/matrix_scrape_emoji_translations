b282eb4410aa373310f643d8162ef4631cdbd36b
Sorunome <mail@sorunome.de>
2020-06-25 17:59:10 +0200

    add riot ios scraping

--- scrape.js
+++ scrape.js
@@ -3,9 +3,11 @@ const xml2json = require("xml2json");
 
 const REACT_SDK_PATH = "/home/sorunome/repos/matrix-react-sdk";
 const RIOT_X_PATH = "/home/sorunome/repos/riotX-android";
+const RIOT_IOS_PATH = "/home/sorunome/repos/riot-ios";
 
 const reactSdkTranslationsPath = "/src/i18n/strings";
 const riotXTranslationsPath = "/matrix-sdk-android/src/main/res";
+const riotIosTranslationsPath = "/Riot/Assets";
 const emojiFile = "./sas-emoji.json";
 const outFile = "./sas-emoji-translations.json";
 const outFileLang = "./sas-emoji-languages.json";
@@ -69,6 +71,7 @@ function getEmoteRiotX(lang, str) {
 	str = strMap[str] || str;
 	const langMap = {
 		"en_EN": "en_GB",
+		"de_DE": "de",
 	};
 	lang = langMap[lang] || lang;
 	lang = lang.replace("_", "-r");
@@ -103,6 +106,7 @@ function allRiotXLangs() {
 		lang = lang.replace("-r", "_");
 		const langMap = {
 			"en_GB": "en_EN",
+			"de": "de_DE",
 		};
 		lang = langMap[lang] || lang;
 		langs.push(lang);
@@ -111,13 +115,62 @@ function allRiotXLangs() {
 	return langs;
 }
 
+const riotIosCache = {};
+function getEmoteRiotIos(lang, str) {
+	str = str.toLowerCase();
+	const langMap = {
+		"en_EN": "en",
+		"nb_NO": "nb-NO",
+		"zh_CN": "zh_Hans",
+		"zh_TW": "zh_Hant",
+		"de_DE": "de",
+	};
+	lang = langMap[lang] || lang;
+	if (!riotIosCache[lang]) {
+		let file;
+		try {
+			file = fs.readFileSync(RIOT_IOS_PATH + riotIosTranslationsPath + "/" + lang + ".lproj/Vector.strings");
+		} catch (err) {
+			return;
+		}
+		langMap[lang] = file.toString().split("\n");
+	}
+	const line = langMap[lang].find((s) => s && s.startsWith("\"device_verification_emoji_" + str));
+	if (!line) {
+		return;
+	}
+	return line.split("\"")[3];
+}
+
+function getRiotIosLangs() {
+	const langs = [];
+	const dir = fs.readdirSync(RIOT_IOS_PATH + riotIosTranslationsPath);
+	for (const f of dir) {
+		let lang = f.replace(".lproj", "");
+		if (lang === "Base") {
+			continue;
+		}
+		const langMap = {
+			"en": "en_EN",
+			"nb-NO": "nb_NO",
+			"zh_Hans": "zh_CN",
+			"zh_Hant": "zh_TW",
+			"de": "de_DE",
+		};
+		lang = langMap[lang] || lang;
+		langs.push(lang);
+	}
+	return langs;
+}
+
 function getEmote(lang, str) {
-	return getEmoteReactSdk(lang, str) || getEmoteRiotX(lang, str);
+	return getEmoteReactSdk(lang, str) || getEmoteRiotX(lang, str) || getEmoteRiotIos(lang, str);
 }
 
 const langs = new Set();
 allReactSdkLangs().forEach((l) => langs.add(l));
 allRiotXLangs().forEach((l) => langs.add(l));
+getRiotIosLangs().forEach((l) => langs.add(l));
 for (const lang of langs) {
 	const translation = [];
 	for (const e of emoji) {
